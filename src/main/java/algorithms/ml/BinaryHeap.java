2717053eb2d66e09a754119d40865836dbc3da3e
Maxim Tsoy <mtsoy1990@gmail.com>
2016-01-12 06:49:41 +0400

    Binary heap bad version

--- src/main/java/algorithms/ml/BinaryHeap.java
+++ src/main/java/algorithms/ml/BinaryHeap.java
@@ -23,8 +23,17 @@ public class BinaryHeap {
 
 
     public static void main(String[] args) {
-//        String s = "3 1 2 2 2";
-        String s = "3 4 3 5 6";
+        String s = "3 1 2 2 2"; //4
+        s = "12 1 4 8 9 10 40 45 50 60 70 80 100 100"; //13
+        s = "7 1 3 5 7 9 11 13 25"; //5
+        s = "10 1 1 1 1 1 1 1 1 1 10 11"; //4
+        s = "2 4 1 6"; //3
+        s = "7 1 1 1 1 1 1 1 3"; //3
+        s = "3 4 3 5 6"; //5
+        s = " 10 1 2 3 4 5 6 7 8 9 10 12"; //10
+
+//        5/128 64 32 32 32/192 -> 8
+//        10/6 6 6 6 6 6 6 8 8 8/15 -> 10
         start(new BufferedInputStream(new ByteArrayInputStream(s.getBytes())), System.out);
     }
 
@@ -40,27 +49,32 @@ public class BinaryHeap {
         int i = 0;
         while (a.length > 0) {
             int sum = 0;
-            while (a.length > 0 && sum + a[0] <= m) {
-                sum += a[0];
-                if (a[0] == 1) {
-                    a[0] = a[a.length - 1];
-                    if (a.length > 0) {
-                        a = Arrays.copyOf(a, a.length - 1);
-                    }
-
-                    siftDown(a, 0);
-                } else {
-                    int tmp = a[0] / 2;
-                    a[0] = a[a.length - 1];
-                    if (a.length > 0) {
-                        a = Arrays.copyOf(a, a.length);
-                    }
-                    siftDown(a, 0);
-                    a[a.length - 1] = tmp;
-                    siftUp(a, a.length - 1);
-                }
-
+            int subLength = 0;
+            while (a.length > subLength && sum + a[subLength] <= m) {
+                sum += a[subLength];
+                subLength++;
             }
+            int countZero = 0;
+            for (int j = 0; j < subLength; j++) {
+                a[j] /= 2;
+                if (a[j] == 0) {
+                    countZero++;
+                    if (subLength <= a.length - countZero) {
+                        swap(a, j, a.length - countZero);
+                    }
+                }
+            }
+
+            for (int j = subLength-1; j >= 0; j--) {
+                siftDown(a, j);
+            }
+//            buildHeap(a);
+
+            if (countZero != 0 && a.length - countZero >= 0) {
+                a = Arrays.copyOf(a, a.length - countZero);
+            }
+
+
             i++;
         }
 
@@ -93,6 +107,9 @@ public class BinaryHeap {
         if (largest != i) {
             swap(m, i, largest);
             siftDown(m, largest);
+        }
+        if (left < m.length && right < m.length && m[left] < m[right]) {
+            swap(m, left, right);
         }
     }
 
