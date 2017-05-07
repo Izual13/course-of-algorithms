a91fe30690f1b5d51211d2a739d6b8bae54a55da
Izual <mtsoy1990@gmail.com>
2017-05-07 12:04:47 +0300

    add backpack

---
+++ src/main/java/algorithms/csc/Backpack.java
@@ -0,0 +1,61 @@
+package algorithms.csc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Backpack {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int v = s.nextInt();
+        List<Bag> ranges = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            double a = s.nextDouble();
+            double b = s.nextDouble();
+            ranges.add(new Bag(a, b));
+        }
+        Collections.sort(ranges);
+
+        double cost = 0;
+        for (Bag bag : ranges) {
+            if (v > 0) {
+                if (v - bag.amount > 0) {
+                    cost += bag.cost;
+                    v -= bag.amount;
+                } else {
+                    cost += bag.cost / bag.amount * v;
+                    v = 0;
+                }
+            }
+        }
+
+        System.out.println(String.format("%.3f", cost));
+
+
+    }
+
+    static class Bag implements Comparable {
+        double cost, amount;
+
+        public Bag(double a, double amount) {
+            this.cost = a;
+            this.amount = amount;
+        }
+
+        public int compareTo(Object o) {
+            Bag ip = (Bag) o;
+            return -Double.compare(cost / amount, ip.cost / ip.amount);
+        }
+
+        @Override public String toString() {
+            return "Bag{" +
+                    "cost=" + cost +
+                    ", amount=" + amount +
+                    ", cost/amount=" + cost / amount +
+                    '}';
+        }
+    }
+}
